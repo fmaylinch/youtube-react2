695e48ee89e3571727fe8121db1f833282fa21b1
fmaylinch <ferranmaylinch@gmail.com>
2017-10-24 19:30:12 +0200

    Add type checking with PropTypes

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const VideoItem = (props) => {
 
     return (
         <div> 
-            <img src={props.image} alt={props.title} />
+            <img src={props.video.image} alt={props.video.title} />
         </div>
     )
 }
-    
+
+VideoItem.propTypes = {
+    video: PropTypes.shape({
+        image: PropTypes.string,
+        title: PropTypes.string
+    })
+}
+
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,13 +4,17 @@ import './VideoList.css'
 
 const VideoList = (props) => {
 
-    const videoItems = props.videos.map(v => 
-      <VideoItem
-          key={v.id.videoId}
-          videoId={v.id.videoId}
-          title={v.snippet.title}
-          image={v.snippet.thumbnails.medium.url} />
-        )
+    const videos = props.videos.map(v => (
+        {
+            id: v.id.videoId,
+            title: v.snippet.title,
+            image: v.snippet.thumbnails.medium.url
+        }
+    ))
+
+    const videoItems = videos.map(v => 
+      <VideoItem key={v.id} video={v} />
+    )
 
     return (
         <div className="video-list">{videoItems}</div>
