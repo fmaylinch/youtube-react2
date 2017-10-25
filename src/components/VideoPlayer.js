850c8d05afec8dd236694bc58f9c409d8cd1e07e
fmaylinch <ferranmaylinch@gmail.com>
2017-10-25 17:57:48 +0200

    Get video data in VideoPlayer from Youtube API

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,22 +1,49 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import type { ContextRouter } from 'react-router-dom'
+import axios from 'axios'
 
-const VideoPlayer = (props: ContextRouter) => {
+type State = {
+    videoId: string,
+    title: string
+};
 
-    const videoId = props.match.params.id
+class VideoPlayer extends Component<ContextRouter, State> {
 
-    return (
-        <div> 
-            <iframe 
-                title={videoId}
-                width="560" height="315"
-                src={"https://www.youtube.com/embed/" + videoId}
-                frameBorder="0" allowFullScreen>
-            </iframe>
-        </div>
-    )
+    constructor(props: ContextRouter) {
+        super(props)
+
+        this.state = {
+            videoId: props.match.params.id,
+            title: ""
+        }
+        
+        const url = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + this.state.videoId + "&key=AIzaSyAadYMkvAiefLRUy6H5Or8uOFqyYbl9DcI"
+    
+        axios.get(url).then(r => {
+            const items = r.data.items
+            if (items.length > 0) {
+                const title = items[0].snippet.title
+                this.setState({ title: title})
+            }
+        })
+    
+    }
+
+    render() {
+        return (
+            <div>
+                <div>{this.state.title}</div>
+                <iframe 
+                    title={this.state.videoId}
+                    width="560" height="315"
+                    src={"https://www.youtube.com/embed/" + this.state.videoId}
+                    frameBorder="0" allowFullScreen>
+                </iframe>
+            </div>
+        )
+    }
 }
 
 export default VideoPlayer;
